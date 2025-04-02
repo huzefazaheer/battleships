5a2e7cd46edf5e9f823d6ced59a0b91deb0c1002
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-04-03 02:05:52 +0500

    completed gameboard and testing

--- gameboard.js
+++ gameboard.js
@@ -1,30 +1,73 @@
 function createGameBoard() {
     let Board = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
     let missedAttacks = []
+    let totalShips = 0
 
-    function placeShip(x, y, ship){
-        checkCoords(x)
-        checkCoords(y)
+    function placeShip(y, x, ship, isRotated = false){
 
-        for(let i = 0; i < ship.length; i++){
-            Board[y][x+i] = ship
+        if (isRotated == false){
+            for(let i = 0; i < ship.length; i++){
+                checkCoords(y, x + i)
+                checkIfFree(y, x + i)
+                Board[y][x+i] = ship
+            }
+        }else {
+            for(let i = 0; i < ship.length; i++){
+                checkCoords(y + i, x)
+                checkIfFree(y + i, x)
+                Board[y+i][x] = ship
+            }
+            Board[0][0] == ship
+            Board
         }
+
+        totalShips ++
     }
 
-    function receiveAttack(x, y){
-        const ship = Board[x][y]
-        ship.hit()
-        return true
+    function receiveAttack(y, x){
+        checkCoords(y, x)
+
+        if (hasShip(y, x)){
+            const ship = Board[y][x]
+            ship.hit()
+            if (ship.isSunk()){
+                totalShips --
+            }
+            Board[y][x] = 1
+            return true
+        }else {
+            missedAttacks.push([y, x])
+            return false
+        }
+
+        
     }
 
-    function checkCoords(a){
-        if(a > 9 || a < 0){
+    function checkCoords(a, b){
+        if(a > 9 || a < 0 || b > 9 || b < 0){
             throw new Error("Invalid coordinates")
         }
     }
 
+    function checkIfFree(y, x){
+        if (Board[y][x] == 1){
+            throw new Error('Already hit square')
+        }else if (Board[y][x] != 0){
+            throw new Error('Already occupied square')
+        }
+    }
+
+    function hasShip(y, x){
+        if (Board[y][x] == 0) return false
+        else return true
+    }
+
+    function shipsRemaining(){
+        return totalShips
+    }
+
     return {
-        Board, receiveAttack, placeShip
+        Board, receiveAttack, placeShip, missedAttacks, shipsRemaining
     }
 }
 
--- gameboard.test.js
+++ gameboard.test.js
@@ -13,15 +13,42 @@ test('Place ship at 0,0 which should be displayed', () => {
     expect(gameBoard.Board).toEqual(reqBoard)
 })
 
+test('Coords already have a ship', () => {
+    expect(() => gameBoard.placeShip(0,1,new Ship(2))).toThrow('Already occupied square')
+})
+
+test('Place ship at 1,1 vertically which should be displayed', () => {
+
+    const reqBoard = [[{"health": 2,"hitCount": 0,"length": 2},{"health": 2,"hitCount": 0,"length": 2},0,0,0,0,0,0,0,0],[0,{"health": 2,"hitCount": 0,"length": 2},0,0,0,0,0,0,0,0],[0,{"health": 2,"hitCount": 0,"length": 2},0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
+    gameBoard.placeShip(1,1,new Ship(2), true)
+    expect(gameBoard.Board).toEqual(reqBoard)
+})
+
 test('Hit ship and damage it', () => {
 
-    gameBoard.placeShip(0,0,new Ship(2))
     expect(gameBoard.receiveAttack(0,0)).toBeTruthy()
-    expect(gameBoard.Board[0][0].hitCount).toEqual(1)
+    expect(gameBoard.Board[0][0]).toEqual(1)
     expect(gameBoard.Board[0][1].hitCount).toEqual(1)
+})
+
+test('Check damage of unhit ship', () => {
+
+    expect(gameBoard.Board[1][1].hitCount).toEqual(0)
+    expect(gameBoard.Board[2][1].hitCount).toEqual(0)
 })
 
 test('Hit where there is no ship', () => {
 
-    expect(gameBoard.receiveAttack(0,0)).toBeFalsy()
+    expect(gameBoard.receiveAttack(7,7)).toBeFalsy()
+    expect(gameBoard.missedAttacks.length).toBe(1)
+    expect(gameBoard.shipsRemaining()).toBe(2)
+    expect(gameBoard.missedAttacks).toEqual([[7, 7]])
+})
+
+test('All ships are sunk', () => {
+    gameBoard.receiveAttack(0, 1)
+    gameBoard.receiveAttack(1, 1)
+    gameBoard.receiveAttack(2, 1)
+
+    expect(gameBoard.shipsRemaining()).toBe(0)
 })
