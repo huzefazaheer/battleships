1d4fa48b622e645af187ad8dc69c97fed911053e
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-06-11 23:31:20 +0500

    Updated styling

--- game.js
+++ game.js
@@ -1,7 +1,12 @@
     import Player from "./utils/Player.js"
     import Ship from "./utils/Ship.js"
 
+    const hp1name = document.querySelector(".p1name")
+    const hp2name = document.querySelector(".p2name")
+
+
     let isHorizontal = true
+    let gameFinised = false
 
     const carrier = new Ship("Carrier", 5);
     const battleship = new Ship("Battleship", 4);
@@ -11,8 +16,30 @@
 
     const ships = [carrier, battleship, cruiser, submarine, destroyer]
 
-    const p1 = new Player("Player1");
-    const p2 = new Player("Player2");
+    async function getInputFromPrompt(text){
+        const prompt = document.querySelector(".prompt")
+        const button = document.getElementById("btn")
+        const inputPrompt = document.querySelector(".pinput")
+        const headingPrompt = document.querySelector(".promptheading")
+        headingPrompt.innerText = text
+        prompt.classList.remove("prompthidden")
+        return new Promise(resolve => {
+            const buttonListener = (e) => {
+            let val = inputPrompt.value
+            prompt.classList.add("prompthidden")
+            button.removeEventListener("click", buttonListener)
+            resolve(val)
+        }
+            button.addEventListener("click", buttonListener)
+        })
+    }
+
+    let p1_name = await getInputFromPrompt("Enter player 1 name")
+    let p2_name = await getInputFromPrompt("Enter player 2 name")
+
+
+    const p1 = new Player(p1_name);
+    const p2 = new Player(p2_name);
 
     async function placeUserShips(player){
 
@@ -46,17 +73,14 @@
         
     }
 
-    function displayBoard(gameBoard){
-        let domBoard
-
-        if(gameBoard === p1.gameBoard) domBoard = document.getElementById("p1Board").children[0]
-        else domBoard = document.getElementById("p2Board").children[0]
+    function displayBoard(player){
+        const domBoard = getPlayerBoardDOM(player)
 
         for(let i = 0; i < 10; i++){
             const row = domBoard.children[i]
             for(let j = 0; j < 10; j++){
                 const column = row.children[j]
-                if(gameBoard.board[i][j] != 0){
+                if(player.gameBoard.board[i][j] != 0){
                     column.classList.add("hasship");
                 }
             }
@@ -64,10 +88,7 @@
     }
 
     async function getPlayerPlaceCoords(player){
-        let domBoard
-
-        if(player === p1) domBoard = document.getElementById("p1Board").children[0]
-        else domBoard = document.getElementById("p2Board").children[0]
+        const domBoard = getPlayerBoardDOM(player)
         
         return new Promise(resolve => {
             const clickHandler = (e) => {
@@ -81,7 +102,41 @@
 
     }
 
+    function getPlayerBoardDOM(player){
+        let domBoard
+        if(player === p1) domBoard = document.getElementById("p1Board").children[0]
+        else domBoard = document.getElementById("p2Board").children[0]
+        return domBoard
+    }
+
+    function toggleGameBoard(player){
+        const domBoard = getPlayerBoardDOM(player)
+        if(domBoard.classList.contains("gameboardhidden")) domBoard.classList.remove("gameboardhidden")
+        else domBoard.classList.add("gameboardhidden")
+    }
+
+    async function playTurn(player){
+        const playerCoords = await getPlayerPlaceCoords(player)
+        try{
+            player.gameBoard[playerCoords[0]][playerCoords[1]].hit()
+            console.log("hit")
+        }catch (e){
+            console.log("error")
+            playTurn(player)
+        }
+    }
+
+    hp1name.innerText = p1.name
+    hp2name.innerText = p2.name
+
+    toggleGameBoard(p1)
     await placeUserShips(p1);
     displayBoard(p1.gameBoard)
     await placeUserShips(p2)
     displayBoard(p2.gameBoard)
+    console.log("Attacking Phase")
+
+    // while(!gameFinised){
+    //     playTurn(p1)
+    //     playTurn(p2)
+    // }
--- utils/gameBoard.js
+++ utils/gameBoard.js
@@ -7,19 +7,19 @@ export default class gameBoard{
         let pos = []
         pos[0] = parseInt(Spos[0], 10)
         pos[1] = parseInt(Spos[1], 10)
-        
+
         if(!isVert){
-            if(ship.health+pos[0] > 10) throw new Error("Out of bounds")
+            if(ship.health+pos[0] > 9) throw new Error("Out of bounds")
             for(let i=pos[0]; i<ship.health+pos[0]; i++){
-                if(this.board[i][pos[1]] != 0) throw new Error("Already occupied coordinate")
+                if(this.board[i][pos[1]] != 0) throw new Error("Already occupied coordinate"+ this.board[i][0])
             }
             for(let i=pos[0]; i<ship.health+pos[0]; i++){
                 this.board[i][pos[1]] = ship
             }
         }else{
-            if(ship.health+pos[1] > 10) throw new Error("Out of bounds")
-            for(let i=pos[0]; i<ship.health+pos[0]; i++){
-                if(this.board[pos[0]][i] != 0) throw new Error("Already occupied coordinate")
+            if(ship.health+pos[1] > 9) throw new Error("Out of bounds")
+            for(let i=pos[1]; i<ship.health+pos[1]; i++){
+                if(this.board[pos[0]][i] != 0) throw new Error("Already occupied coordinate"+ i)
             }
             for(let i=pos[1]; i<ship.health+pos[1]; i++){
                 this.board[pos[0]][i] = ship
